e3b04f5109919cc041ade3d924f9dabaefde7035
al4mcgowan <x00080345@ittd.ie>
2014-09-26 15:42:56 +0100

    CPU count on guest

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Title : script.sh
+# Date : 26/09/2014
+# Author : Alan McGowan
+# Version : 1.0
+# Description : CPU count script
+# Options : 
+
 grep processor /proc/cpuinfo | wc -l
